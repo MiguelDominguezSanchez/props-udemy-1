4011b90b33b2e72998c15887b95c44866d245655
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-23 18:09:11 +0100

    48. Alternativa para llamar a los PROPS

--- src/components/Comentario.jsx
+++ src/components/Comentario.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Imagen from './Imagen';
 
-const Comentario = (props) => {
-	console.log(props);
+const Comentario = ({ urlImagen, persona, texto }) => {
+	// console.log(props);
 
 	return (
 		<div className="media">
-			<Imagen urlImagen={props.urlImagen} />
+			<Imagen urlImagen={urlImagen} />
 			<div className="media-body">
-				<h5 className="mt-0">{props.persona}</h5>
-				{props.texto}
+				<h5 className="mt-0">{persona}</h5>
+				{texto}
 			</div>
 		</div>
 	);
--- src/components/Imagen.jsx
+++ src/components/Imagen.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
-const Imagen = (props) => {
-	return (
-		<img src={props.urlImagen} className="mr-3" width="64" height="64" alt="" />
-	);
+const Imagen = ({ urlImagen }) => {
+	return <img src={urlImagen} className="mr-3" width="64" height="64" alt="" />;
 };
 
 export default Imagen;
