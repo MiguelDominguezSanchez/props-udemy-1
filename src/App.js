9832c4d31d4a8173fe44493928b284d64be3113e
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-01-23 17:18:06 +0100

    46. PROPS - Componentes dinámicos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Saludo from './components/Saludo';
 
 function App() {
 	return (
-		<div>
+		<div className="container mt-5">
 			<h1>Proyecto desde cero</h1>
+			<Saludo persona="Ignacio" edad={30} />
+			<Saludo persona="Juanito" edad={25} />
+			<Saludo persona="Pedrito" edad={35} />
 		</div>
 	);
 }
